589ffed3a92bd31dda5baa373429a449d1af73f1
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-03-28 16:08:40 +0900

    Initial Commit

---
+++ AVFoundationExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVFoundationExamples
+//
+//  Created by TokyoYoshida on 2021/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
